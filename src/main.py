0c7492089dfd12ec4665ed79cde433b7475c9502
kaansenol5 <moonman76@protonmail.com>
2019-04-14 14:08:07 +0300

    first commit

--- src/main.py
+++ src/main.py
@@ -1,44 +1,60 @@
 import discord
-import pickle
+
 from login import r, token
+
 client=discord.Client()
 used=[]
-def save():
-    with open("used","ab") as f:
-        pickle.dump(used,f)
-def load():
-    with open("used","rb") as f:
-        used=pickle.load(f)
-load()
+f = open("used", "wb")
+helpmsg = """
+?meme for a meme
+?wallpaper for a wallpaper
+?copypasta for copypasta
+?tifu for a tifu story
+?help to display this message
+?img/r/* for an image/gif from a subreddit
+?txt/r/* for a text from a subreddit"""
 
 def sub(x):
     return r.subreddit(x)
 
-def get_post_reddit(subreddit, type):
+
+def get_post_reddit(subr, type):
+    global used
+    print("peepee")
+    subreddit = sub(subr)
     for submission in subreddit.hot():
-        if submission.url not in used and not submission.stickied:
+        if submission.url not in used and not submission.stickied and not submission.over_18:
             used.append(submission.url)
             if type == "img":
                 return submission.url
             if type == "txt":
                 return submission.title + "\n\n\n" + submission.selftext
 
+
+async def sendMsg(command, subreddit, message, type, reddit, text=None):
+    if message.content == command:
+        if reddit:
+            msg = get_post_reddit(subreddit, type)
+            print(command)
+            await client.send_message(message.channel, msg)
+        else:
+            await client.send_message(message.channel, text)
 @client.event
 async def on_message(message):
-    if message.content == "?meme":
-        msg=get_post_reddit(sub("me_irl"),"img")
-        print("sending meme")
-        await client.send_message(message.channel,msg)
-    if message.content == "?wallpaper":
-        msg=get_post_reddit(sub("wallpapers"),"img")
-        print("sending wallpaper")
-        await client.send_message(message.channel,msg)
-    if message.content == "?copypasta":
-        print("sending copypasta")
-        msg = get_post_reddit(sub("copypasta"),"txt")
-        await client.send_message(message.channel, msg)
-    if message.content == "?tifu":
-        print("sending tifu")
-        msg = get_post_reddit(sub("tifu"),"txt")
-        await client.send_message(message.channel, msg)
+    await sendMsg("?meme", "me_irl", message, "img", True)
+    await sendMsg("?wallpaper", "wallpapers", message, "img", True)
+    await sendMsg("?tifu", "tifu", message, "txt", True)
+    await sendMsg("?copypasta", "copypasta", message, "txt", True)
+    await sendMsg("?help", None, message, None, False, text=helpmsg)
+    if "?txt/r/" in message.content:
+        subreddit = message.content.replace("?txt/r/", "")
+        await client.send_message(message.channel, get_post_reddit(subreddit, "txt"))
+    if "img/r/" in message.content:
+        subreddit = message.content.replace("?img/r/", "")
+        await client.send_message(message.channel, get_post_reddit(subreddit, "img"))
+    await sendMsg("i will hack ur pc", None, message, None, False,
+                  text="Pleas no hack my pc my ip is 127.0.0.1 my mom just buyed this computer have mercy")
+    await sendMsg("Özgür is a retard", None, message, None, False, text="Yes he is")
+    await sendMsg("?cringe", "criingetopia", message, "img", True)
+    await sendMsg("?code", None, message, None, False, text="")
 client.run(token)
